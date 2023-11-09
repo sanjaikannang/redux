059f92a04f43935211e0ddf57d1969efebc9b8a5
sanjaikannang <sanjaikannang@gmail.com>
2023-11-09 10:56:55 +0530

    secondcomit

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import store from "./store/store";
+// import store from "./store/store";
 import "./index.css";
+import Store from "./store/store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  <Provider store={Store}>
     <App />
   </Provider>
 );
